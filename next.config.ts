710fc1c91e11730a7ebe975434d26556cf424d6a
tuntun <760523867@qq.com>
2025-01-20 00:02:52 +0800

    feat: init contentlayer

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,11 @@
 import type { NextConfig } from 'next'
 import withBundleAnalyzer from '@next/bundle-analyzer'
+import { withContentlayer } from 'next-contentlayer2'
 
 const nextConfig: NextConfig = {
   /* config options here */
 }
 
 export default withBundleAnalyzer({ enabled: process.env.ANALYZE === 'true' })(
-  nextConfig
+  withContentlayer(nextConfig)
 )
